186c2e7090d5495f440e96bc74d7eb6cb4cb6aa5
Shreemanarjun <shreemanarjunsahu@gmail.com>
2021-06-07 08:29:53 +0530

    Scrollbar plugin depreceated

--- lib/Pages/MyHomePage.dart
+++ lib/Pages/MyHomePage.dart
@@ -1,11 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:vs_scrollbar/vs_scrollbar.dart';
+import 'package:my_protofolio/UIs/FooterWidget.dart';
+import 'package:my_protofolio/UIs/MiddleWidget.dart';
 
 import '../Controllers/ThemeController.dart';
-import '../UIs/FooterWidget.dart';
 import '../UIs/HeaderWidget.dart';
-import '../UIs/MiddleWidget.dart';
 
 class MyHome extends StatelessWidget {
   final ThemeController themeController = Get.find();
@@ -21,23 +20,12 @@ class MyHome extends StatelessWidget {
         preferredSize: Size(Get.width, 150),
         child: Header(),
       ),
-      body: VsScrollbar(
+      body: SingleChildScrollView(
         controller: scrollController,
-        scrollDirection: Axis.vertical,
-        allowDrag: true,
-        color: Colors.indigo,
-        radius: 60,
-        thickness: 10,
-        isAlwaysShown: true,
-        scrollbarFadeDuration: Duration(milliseconds: 500),
-        scrollbarTimeToFade: Duration(milliseconds: 800),
-        child: SingleChildScrollView(
-          controller: scrollController,
-          child: Column(
-            children: [
-              MiddleWidget(),
-            ],
-          ),
+        child: Column(
+          children: [
+            MiddleWidget(),
+          ],
         ),
       ),
       bottomNavigationBar: Footer(),
